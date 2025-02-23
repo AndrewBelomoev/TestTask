074220ebd49407a88466cf90df2e22e3109cb175
AndrewBelomoev <andrewbelomoev@gmail.com>
2025-02-23 11:00:09 +0300

    Enhance Error Handling, Data Refresh, and UI Improvements - Added LceState for error handling (show message when API is unavailable) and loading state. - Introduced NetworkUtils for internet connection checking. - Integrated SwipeRefreshLayout for seamless data refreshing. - Enhanced the user interface. - Implemented Snackbar for user notifications. - Fixed several issues and improved overall functionality.

--- app/src/main/java/com/example/testtask/presentation/ui/users/UsersViewModel.kt
+++ app/src/main/java/com/example/testtask/presentation/ui/users/UsersViewModel.kt
@@ -4,27 +4,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.models.user.User
 import com.example.domain.usecase.GetUsersUseCase
+import com.example.testtask.presentation.model.LceState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class UsersViewModel(private val getUsersUseCase: GetUsersUseCase) : ViewModel() {
-    private val _users = MutableStateFlow<List<User>>(emptyList())
-    val users: StateFlow<List<User>> get() = _users
-
+    private val _usersState = MutableStateFlow<LceState<List<User>>>(LceState.Loading)
+    val usersState: StateFlow<LceState<List<User>>> get() = _usersState
 
     init {
         fetchUsers()
     }
 
-    private fun fetchUsers() {
+    fun fetchUsers() {
         viewModelScope.launch {
+            _usersState.value = LceState.Loading
+            delay(300)
             val result = getUsersUseCase()
-            if (result.isSuccess) {
-                _users.value = result.getOrNull() ?: emptyList()
-            } else {
-                // Error Handling
-            }
+            _usersState.value = result.fold(
+                onSuccess = { users -> LceState.Content(users) },
+                onFailure = { throwable -> LceState.Error(throwable) }
+            )
         }
     }
 }
--- data/src/main/java/com/example/data/models/UserDTO.kt
+++ data/src/main/java/com/example/data/models/UserDTO.kt
@@ -1,6 +1,6 @@
 package com.example.data.models
 
-data class UserDTO(
+internal data class UserDTO(
     val id: Int? = null,
     val name: String? = null,
     val username: String? = null,
@@ -11,7 +11,7 @@ data class UserDTO(
     val company: CompanyDTO? = null
 )
 
-data class AddressDTO(
+internal data class AddressDTO(
     val street: String? = null,
     val suite: String? = null,
     val city: String? = null,
@@ -19,12 +19,12 @@ data class AddressDTO(
     val geo: GeoDTO? = null
 )
 
-data class GeoDTO(
+internal data class GeoDTO(
     val lat: String? = null,
     val lng: String? = null
 )
 
-data class CompanyDTO(
+internal data class CompanyDTO(
     val name: String? = null,
     val catchPhrase: String? = null,
     val bs: String? = null
